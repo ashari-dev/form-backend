6700a0d65579329a6ae66693bf688e37296de4e7
ashari-dev <boykid923@gmail.com>
2025-01-08 22:19:53 +0700

    feat: CRUD questioner

---
+++ controllers/QuestionsControllers.js
@@ -0,0 +1,175 @@
+import mongoose from "mongoose";
+import Form from "../models/Form.js";
+
+const allowedTypes = ["Text", "Radio", "Checkbox", "Dropdown", "Email"];
+class QuestionsController {
+  async index(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: "Required form id" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "Invalid id" };
+      }
+
+      const form = await Form.findOne({
+        _id: req.params.id,
+        userId: req.jwt.id,
+      });
+
+      if (!form) {
+        throw { code: 404, message: "Form not found" };
+      }
+
+      return res.status(200).json({
+        status: true,
+        message: "Questions retrieved successfully",
+        data: form,
+      });
+    } catch (error) {
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
+  async store(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: "Required form id" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "Invalid id" };
+      }
+
+      const newQuestion = {
+        id: new mongoose.Types.ObjectId(),
+        question: null,
+        type: "text",
+        required: false,
+        options: req.body.options,
+      };
+
+      const form = await Form.findOneAndUpdate(
+        { _id: req.params.id, userId: req.jwt.id },
+        { $push: { question: newQuestion } },
+        { new: true }
+      );
+      if (!form) {
+        throw { code: 404, message: "Question not found" };
+      }
+      return res.status(200).json({
+        status: true,
+        message: "Question added successfully",
+        data: { question: newQuestion },
+      });
+    } catch (error) {
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: "Required form id" };
+      }
+      if (!req.params.questionId) {
+        throw { code: 400, message: "Required question id" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "Invalid id" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.questionId)) {
+        throw { code: 400, message: "Invalid question id" };
+      }
+
+      let field = {};
+
+      if (req.body.hasOwnProperty("question")) {
+        field["question.$[indexQuestion].question"] = req.body.question;
+      } else if (req.body.hasOwnProperty("required")) {
+        field["question.$[indexQuestion].required"] = req.body.required;
+      } else if (req.body.hasOwnProperty("type")) {
+        if (!allowedTypes.includes(req.body.type)) {
+          throw { code: 400, message: "Invalid question type" };
+        }
+        field["question.$[indexQuestion].type"] = req.body.type;
+      }
+
+      const question = await Form.findOneAndUpdate(
+        { _id: req.params.id, userId: req.jwt.id },
+        { $set: field },
+        {
+          arrayFilters: [
+            {
+              "indexQuestion.id": new mongoose.Types.ObjectId(
+                req.params.questionId
+              ),
+            },
+          ],
+          new: true,
+        }
+      );
+      if (!question) {
+        throw { code: 404, message: "Question not found" };
+      }
+
+      return res.status(200).json({
+        status: true,
+        message: "Question updated successfully",
+        data: { question },
+      });
+    } catch (error) {
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
+  async destroy(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: "Required form id" };
+      }
+      if (!req.params.questionId) {
+        throw { code: 400, message: "Required question id" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "Invalid id" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.questionId)) {
+        throw { code: 400, message: "Invalid question id" };
+      }
+
+      const form = await Form.findOneAndUpdate(
+        { _id: req.params.id, userId: req.jwt.id },
+        {
+          $pull: {
+            question: {
+              id: new mongoose.Types.ObjectId(req.params.questionId),
+            },
+          },
+        },
+        { new: true }
+      );
+      if (!form) {
+        throw { code: 404, message: "Question not found" };
+      }
+      return res.status(200).json({
+        status: true,
+        message: "Delete question successfully",
+        data: { form },
+      });
+    } catch (error) {
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
+}
+
+export default new QuestionsController();
--- routers/api.js
+++ routers/api.js
@@ -2,6 +2,7 @@ import e from "express";
 import jwtAuth from "../middlewares/jwtAuth.js";
 import AuthController from "../controllers/AuthControllers.js";
 import FormController from "../controllers/FormControllers.js";
+import QuestionsController from "../controllers/QuestionsControllers.js";
 
 const router = e.Router();
 //auth
@@ -17,5 +18,19 @@ router.post("/forms", jwtAuth(), FormController.store);
 router.get("/forms/:id", jwtAuth(), FormController.show);
 router.put("/forms/:id", jwtAuth(), FormController.update);
 router.delete("/forms/:id", jwtAuth(), FormController.destroy);
+
+// question
+router.get("/forms/:id/question", jwtAuth(), QuestionsController.index);
+router.post("/forms/:id/question", jwtAuth(), QuestionsController.store);
+router.put(
+  "/forms/:id/question/:questionId",
+  jwtAuth(),
+  QuestionsController.update
+);
+router.delete(
+  "/forms/:id/question/:questionId",
+  jwtAuth(),
+  QuestionsController.destroy
+);
 
 export default router;
